3f498cfae90b892b0c4198578791b359ff4586e1
HYOBIN <hbin12212@gmail.com>
2024-02-26 16:26:07 +0900

    [INIT] : init prj1

---
+++ prj1/src/panda.js
@@ -0,0 +1,22 @@
+const API_URL = 'https://animal-api-two.vercel.app/';
+
+const $content = document.querySelector('div.content');
+let template = [];
+
+//API
+const getData = async (name) => {
+    let res = await fetch(`${API_URL}${name}`);
+    try {
+        if (res) {
+            let data = await res.json();
+            data.photos.forEach((elm) => {
+                template += `<img src="${elm.url}"></img>`;
+            });
+            $content.innerHTML = template;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+getData('panda');
